cb8756a9559774ffd995bff7ebcefd3e2db1f191
Alexandre Perrot <alexandre.perrot54@gmail.com>
2023-02-02 19:01:01 +0100

    add etape 2

--- js/notes.js
+++ js/notes.js
@@ -37,16 +37,69 @@ let noteFormView = {
         let noteForm = document.getElementById("noteForm");
         noteForm.classList.add("create_edit_note-hidden");
     },
-    validate(event){
+    validate(){
         noteFormView.hide();
         let formTitreValue = document.getElementById("form_add_note_title").value;
         let formContenuValue = document.getElementById("form_add_note_text").value;
         let note = new Note(formTitreValue, formContenuValue);
         let noteView = new NoteView(note);
         noteView.afficherNoteCourante();
+        app.noteCourante = app.listeNotes.addNote(note);
+        noteListMenuView.displayItem(note);
     }
 
 };
+
+function NoteList(){
+    this.liste = [];
+}
+
+NoteList.prototype.addNote = function (note){
+    return this.liste.push(note) - 1;
+}
+
+NoteList.prototype.getNoteById = function (id){
+    return this.liste[id];
+}
+
+NoteList.prototype.getList = function (){
+    return this.liste;
+}
+
+let noteListMenuView = {
+    displayItem(note){
+        let noteListMenu = document.getElementById("noteListMenu");
+        let div = document.createElement("div");
+        div.setAttribute("class", "note_list_item");
+        let para = document.createElement("p");
+        para.setAttribute("id", `${app.listeNotes.liste.length-1}`);
+        let text = document.createTextNode(`${note.titre} ${note.date_creation.toLocaleString('fr-FR', {timeZone: 'UTC'})}`);
+        para.appendChild(text);
+        div.appendChild(para);
+        noteListMenu.appendChild(div);
+    },
+    selectItem(event){
+        let target = event.target;
+        if(target.nodeName === "SECTION"){
+            return;
+        } else if(target.nodeName === "DIV"){
+            target = target.firstElementChild;
+        }
+        noteListMenuView.unselectItem();
+        app.noteCourante = parseInt(target.id);
+        let note = app.listeNotes.getNoteById(app.noteCourante);
+        let noteItem = document.getElementById(app.noteCourante).parentNode;
+        noteItem.classList.add("note_list_item-selected");
+        let noteView = new NoteView(note);
+        noteView.afficherNoteCourante();
+    },
+    unselectItem(){
+        let noteItems = document.getElementsByClassName("note_list_item-selected");
+        Array.prototype.forEach.call(noteItems, noteItem => {
+            noteItem.classList.remove("note_list_item-selected");
+        });
+    }
+}
 
 let mainMenuView = {
     addHandler(){
@@ -57,10 +110,13 @@ let mainMenuView = {
         add.addEventListener('click', this.addHandler)
         let validate = document.getElementById("form_add_note_valid");
         validate.addEventListener('click', noteFormView.validate);
+        let noteListMenu = document.getElementById("noteListMenu");
+        noteListMenu.addEventListener('click', noteListMenuView.selectItem);
     }
 };
 
 let app = {
+    listeNotes : new NoteList(),
     noteCourante : undefined,
     init(){
         mainMenuView.init();
